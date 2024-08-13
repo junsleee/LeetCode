999b961a23415aa26720ad6c5deeb51a9d35c752
junsleee <junslee1204@gmail.com>
2024-08-13 15:44:04 -0700

    fourth easy leetcode problem - in progress

---
+++ LeetCodeTest/MajorityElementTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    public class MajorityElementTests
+    {
+        [TestMethod()]
+        public void MajorityTest()
+        {
+            //arrange
+            
+
+            //act
+            
+
+            //assert
+        }
+    }
+}
