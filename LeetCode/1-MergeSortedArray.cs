436af0a37944886e7009d866ae7b297392a816c5
junsleee <junslee1204@gmail.com>
2024-08-13 11:02:30 -0700

    first easy leetcode problem

---
+++ LeetCode/1-MergeSortedArray.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace LeetCode
+{ //https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+    public class MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (m == 0) { Array.Copy(nums2, nums1, nums2.Length); return; }
+
+            for (int i = nums1.Length - 1; i >= 0; i--)
+            {
+                if (nums1[m - 1] > nums2[n - 1])
+                {
+                    (nums1[i], nums1[m - 1]) = (nums1[m - 1], nums1[i]);
+                    m -= 1;
+                }
+                else
+                {
+                    (nums1[i], nums2[n - 1]) = (nums2[n - 1], nums1[i]);
+                    n -= 1;
+                    if (n == 0) break;
+                }
+            }
+        }
+    }
+}
+/* nums1 = [1, 2, 3, 0, 0, 0]
+ * m = 3
+ * nums2 = [2, 5, 6]
+ * n = 3
+ * merging [1, 2, 3] , [2, 5, 6]
+ * output: nums1 = [1, 2, 2, 3, 5 ,6]
+ */
+
---
+++ LeetCodeTest/MergeSortedArrayTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class MergeSortedArrayTests
+    {
+        [TestMethod()]
+        public void MergeTest()
+        {
+            //arrange
+            MergeSortedArray m = new MergeSortedArray();
+            //act
+            int[] nums1 = [1, 2, 3, 0, 0, 0];
+            int[] nums2 = [2, 5, 6];
+            int[] expected = [1, 2, 2, 3, 5, 6];
+            m.Merge(nums1, 3, nums2, 3);
+            //assert
+            CollectionAssert.AreEqual(expected, nums1);
+        }
+        
+        //[TestMethod()]
+        //public void MergeTest2()
+        //{
+        //    //arrange
+        //    MergeSortedArray m = new MergeSortedArray();
+        //    //act
+        //    int[] nums1 = [1];
+        //    int[] nums2 = [0];
+        //    int[] expected = [1];
+        //    m.Merge(nums1, 1, nums2, 0);
+        //    //assert
+        //    CollectionAssert.AreEqual(expected, nums1);
+        //}
+        [TestMethod()]
+        public void MergeTest3()
+        {
+            //arrange
+            MergeSortedArray m = new MergeSortedArray();
+            //act
+            int[] nums1 = [0];
+            int[] nums2 = [1];
+            int[] expected = [1];
+            m.Merge(nums1, 0, nums2, 1);
+            //assert
+            CollectionAssert.AreEqual(expected, nums1);
+        }
+    }
+}
