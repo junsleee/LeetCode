d2892b6361c3e14acb04a88c2d61eef612184070
junsleee <junslee1204@gmail.com>
2024-08-13 13:19:35 -0700

    second easy leetcode problem

---
+++ LeetCode/2-RemoveElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveElement
+    {
+        public int Remove(int[] nums, int val)
+        {
+            int x = 0;
+
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                if (nums[i] == val) continue;
+                else nums[x] = nums[i];
+                x += 1;
+            }
+            return x;
+        }
+    }
+}
---
+++ LeetCodeTest/RemoveElementTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class RemoveElementTests
+    {
+        [TestMethod()]
+        public void removeElementTest()
+        {
+            //arrange
+            RemoveElement r = new RemoveElement();
+            //act
+            int[] num1 = [3, 2, 2, 3];
+            int val = 3;
+            int[] expected = [2, 2];
+            int attempt = r.Remove(num1, val);
+            //assert
+            Assert.AreEqual(2, attempt);
+            CollectionAssert.AreEqual(expected, num1[0..attempt]);
+        }
+    }
+}
