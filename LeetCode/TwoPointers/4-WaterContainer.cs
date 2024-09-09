59b4d97e879bc73b5b42201a0cc91eec14899a3c
JunSeob Lee <junslee1204@gmail.com>
2024-09-09 11:49:04 -0700

    medium LC water container

---
+++ LeetCode/TwoPointers/4-WaterContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TwoPointers
+{
+    public class WaterContainer
+    {
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxArea = -1;
+
+            while (left < right)
+            {
+                int area = Math.Min(height[left], height[right]) * (right - left);
+
+                maxArea = Math.Max(maxArea, area);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
---
+++ LeetCodeTest/TwoPointers/_4_WaterContainerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.TwoPointers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TwoPointers.Tests
+{
+    [TestClass()]
+    public class WaterContainerTests
+    {
+        [TestMethod()]
+        public void MaxAreaTest()
+        {
+            WaterContainer waterContainer = new WaterContainer();
+
+            int[] height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+            int expected = 49;
+            int actual = waterContainer.MaxArea(height);
+
+            Assert.AreEqual(49, actual);
+        }
+    }
+}
