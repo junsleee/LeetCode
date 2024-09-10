ee593ec14077bffd9d1d3927fba56774f41c2840
JunSeob Lee <junslee1204@gmail.com>
2024-09-10 00:04:46 -0700

    more leetcode

---
+++ LeetCode/SlidingWindow/MinimumSizeSubarraySum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SlidingWindow
+{
+    public class MinimumSizeSubarraySum
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int start = 0;
+            int end = 0;
+            int windowSum = nums[0];
+            int maxLen = int.MaxValue;
+
+            while (end < nums.Length)
+            {
+                if (windowSum < target)
+                {
+                    end++;
+                    if (end < nums.Length)
+                    {
+                        windowSum += nums[end];
+                    }
+                }
+                else
+                {
+                    maxLen = Math.Min(maxLen, end - start + 1);
+                    windowSum -= nums[start];
+                    start++;
+                }
+            }
+            return maxLen == int.MaxValue ? 0 : maxLen;
+        }
+    }
+}
---
+++ LeetCodeTest/SlidingWindow/MinimumSizeSubarraySumTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.SlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SlidingWindow.Tests
+{
+    [TestClass()]
+    public class MinimumSizeSubarraySumTests
+    {
+        [TestMethod()]
+        public void MinSubArrayLenTest()
+        {
+            var solution = new MinimumSizeSubarraySum();
+            int target = 7;
+            int[] nums = [2, 3, 1, 2, 4, 3];
+
+            int result = solution.MinSubArrayLen(target, nums);
+
+            Assert.AreEqual(target, result);
+        }
+    }
+}
